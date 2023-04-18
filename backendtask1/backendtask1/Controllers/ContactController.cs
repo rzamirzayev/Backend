4d2fd5f358d72ff024129429b579b0c7d2e94aa7
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2023-04-18 15:55:37 +0400

    Add files via upload

---
+++ backendtask1/backendtask1/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backendtask1.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backendtask1/backendtask1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backendtask1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ContentResult Detail(int id)
+        {
+            return Content(id.ToString());
+        }
+    }
+}
